ab53b12df41ec04b4763f0ed5123038c292f0fba
Sanad Ashour <sanadashour2002@yahoo.com>
2025-07-31 15:56:00 +0300

    MainWindow.xaml.cs

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,4 @@
-﻿using System.Text;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -21,7 +21,15 @@ namespace T0_Do_List
             InitializeComponent();
             ShowData();
         }
+        private int selectedId = 0;
         private AppDbContext ADC = new AppDbContext();
+
+        private void ClearData()
+        {
+            Title.Clear();
+            Description.Clear();
+            TaskDate.SelectedDate = null;
+        }
 
         private void ShowData()
         {
@@ -38,13 +46,51 @@ namespace T0_Do_List
                 });
             
             ADC.SaveChanges();
-
-            Title.Clear();
-            Description.Clear();
-            TaskDate.SelectedDate = null;
-
-            ShowData();
+            ClearData();
             MessageBox.Show("ITEMS ADDED!","CONFIRMATION",MessageBoxButton.OK, MessageBoxImage.Information);
+            ShowData();
+        }
+
+        private void TaskList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if(TaskList.SelectedItem is TodoItem item)
+            {
+                Title.Text = item.Title;
+                Description.Text = item.Description;
+                TaskDate.SelectedDate = item.TaskDate;
+                State.Text = item.Status;
+                selectedId = item.Id;
+            }
+        }
+
+        private void updatebtn_Click(object sender, RoutedEventArgs e)
+        {
+            if(selectedId != 0)
+            {
+            var item = ADC.TodoItem.Find(selectedId);
+            item.Title = Title.Text;
+            item.Description = Description.Text;
+            item.TaskDate = TaskDate.SelectedDate;
+            item.Status = State.Text;
+            ADC.TodoItem.Update(item);
+            ADC.SaveChanges();
+            ClearData();
+            MessageBox.Show("ITEMS UPDATED!", "CONFIRMATION", MessageBoxButton.OK, MessageBoxImage.Information);
+            ShowData();
+            }
+        }
+
+        private void deletebtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (selectedId != 0)
+            {
+                var item = ADC.TodoItem.Find(selectedId);
+                ADC.TodoItem.Remove(item);
+                ADC.SaveChanges();
+                ClearData();
+                MessageBox.Show("ITEMS DELETED!", "CONFIRMATION", MessageBoxButton.OK, MessageBoxImage.Information);
+                ShowData();
+            }
         }
     }
 }
