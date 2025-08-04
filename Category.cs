a9d76112eedda4d47b5d16366e9a99f7c6d813e3
Sanad Ashour <sanadashour2002@yahoo.com>
2025-08-04 19:46:11 +0300

    Add files via upload

---
+++ Category.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T0_Do_List
+{
+    class Category
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+
+        public List<TodoItem> TodoItems { get; set; } = new List<TodoItem>();
+
+    }
+}
---
+++ CategoryWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using static System.Windows.Forms.AxHost;
+
+namespace T0_Do_List
+{
+    /// <summary>
+    /// Interaction logic for CategoryWindow.xaml
+    /// </summary>
+    public partial class CategoryWindow : Window
+    {
+        public CategoryWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void addbtn_Click(object sender, RoutedEventArgs e)
+        {
+           
+        }
+
+        private void deletebtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
