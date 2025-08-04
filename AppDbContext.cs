d5911e32342b55849e9a7d462df916ce5c30a24c
Sanad Ashour <sanadashour2002@yahoo.com>
2025-08-05 00:03:04 +0300

    AppDbContext.cs

--- AppDbContext.cs
+++ AppDbContext.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,9 +10,22 @@ namespace T0_Do_List
     class AppDbContext: DbContext
     {
         public DbSet<TodoItem> TodoItem { get; set; }
+        public DbSet<Category> Categories { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer("server=Sanad_HpEnvy\\SQLEXPRESS;database=Tasks;integrated security=true;TrustServerCertificate=true");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            //Configuring one to many relationship
+            modelBuilder.Entity<Category>()
+                .HasMany(c => c.TodoItems) //a category has many todoitems
+                .WithOne(t => t.Category) //a todoitem has one category
+                .HasForeignKey(t => t.CategoryId); //the foreign key is in todoitem
+
         }
     }
 }
